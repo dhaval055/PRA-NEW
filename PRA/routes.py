31a29c8bcf10993afc3f5e9627bc8dd714e01f46
dhaval055 <dhavaldave055@gmail.com>
2020-01-06 16:42:17 +0530

    new template,auth,dashboard

--- PRA/routes.py
+++ PRA/routes.py
@@ -1,12 +1,25 @@
 from flask import render_template, url_for, flash, redirect, request, abort
 from PRA import app
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/layout')
-def layout():
-    return render_template('layout.html')
+@app.route('/registration')
+def registration():
+    return render_template('auth-register.html')
 
+@app.route('/login')
+def login():
+    return render_template('auth-login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')     
+
+@app.route('/update')
+def update():
+    return render_template('update-profile.html')      
+
+    
 
